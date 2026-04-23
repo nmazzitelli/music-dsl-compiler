963d66af182a10570fb1126c77bf48e06f48f51a
nmazzitelli <nmazzitelli@itba.edu.ar>
2026-04-23 18:29:15 -0300

    add chord note, event and event list destructors

--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
@@ -54,6 +54,58 @@ void destroyExpression(Expression * expression) {
 	}
 }
 
+void destroyChordNoteList(ChordNoteList * list) {
+	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
+	while (list != NULL) {
+		ChordNoteList * next = list->next;
+		free(list->pitch);
+		free(list);
+		list = next;
+	}
+}
+
+void destroyEvent(Event * event) {
+	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
+	if (event != NULL) {
+		switch (event->type) {
+			case EVENT_NOTE:
+				free(event->note.pitch);
+				destroyExpression(event->note.velocity);
+				break;
+			case EVENT_CHORD:
+				destroyChordNoteList(event->chord.notes);
+				destroyExpression(event->chord.velocity);
+				break;
+			case EVENT_REST:
+				break;
+			case EVENT_REPEAT:
+				destroyExpression(event->repeat.count);
+				destroyEventList(event->repeat.body);
+				break;
+			case EVENT_IF:
+				destroyExpression(event->ifStatement.condition);
+				destroyEventList(event->ifStatement.thenBody);
+				destroyEventList(event->ifStatement.elseBody);
+				break;
+			case EVENT_VAR_DECL:
+				free(event->varDecl.name);
+				destroyExpression(event->varDecl.value);
+				break;
+		}
+		free(event);
+	}
+}
+
+void destroyEventList(EventList * list) {
+	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
+	while (list != NULL) {
+		EventList * next = list->next;
+		destroyEvent(list->event);
+		free(list);
+		list = next;
+	}
+}
+
 void destroyProgram(Program * program) {
 	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
 	if (program != NULL) {
